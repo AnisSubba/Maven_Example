72d0d304624bbe857710436d185c30642103e4a9
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 12:25:43 +0100

    this is the App class

---
+++ src/main/java/com/spartaglobal/mavenexample/App.java
@@ -0,0 +1,18 @@
+package com.spartaglobal.mavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+        JavaAPIExample javaAPIExample = new JavaAPIExample();
+        javaAPIExample.mathExamples();
+
+        javaAPIExample.dateTime();
+
+
+    }
+}
