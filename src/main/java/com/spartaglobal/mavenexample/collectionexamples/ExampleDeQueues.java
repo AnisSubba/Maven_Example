e861bcfd284f0c012761620f757865cad9f92a3e
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 14:46:16 +0100

    2nd commit in Dequeue using poll, peek

--- src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleDeQueues.java
+++ src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleDeQueues.java
@@ -1,4 +1,23 @@
 package com.spartaglobal.mavenexample.collectionexamples;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class ExampleDeQueues {
-}
+
+    private Deque<String> capPark = new LinkedList<>();
+
+    public ExampleDeQueues(){
+        capPark.add("car1");
+        capPark.add("car2");
+        capPark.add("car3");
+    }
+
+    public void workingWithOurDeQueue(){
+      /*  for(String car : capPark){
+            System.out.println(car);*/
+        System.out.println(capPark.poll());
+        System.out.println(capPark.peek());
+        }
+    }
+
