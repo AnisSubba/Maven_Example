990cb80ad8cbd8bebd99de57718b83baa62cbe49
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 13:02:16 +0100

    This is the list class which uses a Iterator

--- src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleLists.java
+++ src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleLists.java
@@ -1,4 +1,29 @@
 package com.spartaglobal.mavenexample.collectionexamples;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ExampleLists {
+
+    private List<String> hobbies = new ArrayList<>();
+
+    public ExampleLists(List<String> hobbies){
+        this.hobbies = hobbies;
+    }
+
+    public void workingWithOutList(){
+        hobbies.add("MMA");
+        hobbies.add("Football");
+        hobbies.add("Cooking");
+        hobbies.add("Coding");
+    }
+
+    public void printList(){
+        System.out.println("we're using the iterator ..... ");
+        for (Iterator<String> iterator = hobbies.iterator(); iterator.hasNext();){
+            String element = iterator.next();
+            System.out.println(element);
+        }
+    }
 }
