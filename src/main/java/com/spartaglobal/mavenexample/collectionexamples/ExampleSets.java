26717ca404bdd8e7bb2996f33cfaa8b08d31fb9e
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 14:02:41 +0100

    1 commit on set and hashset

--- src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleSets.java
+++ src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleSets.java
@@ -1,4 +1,22 @@
 package com.spartaglobal.mavenexample.collectionexamples;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExampleSets {
+
+    private Set<String> newSet = new HashSet<String>();
+
+public void workingWithOutSet(){
+    newSet.add("milk");
+    newSet.add("meat");
+    newSet.add("rice");
+}
+public void printSet(){
+    System.out.println(newSet.contains("meat"));
+
+    for (String shoppingItem: newSet){
+        System.out.println(shoppingItem);
+    }
+}
 }
