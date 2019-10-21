f6515b47aeac6677103790f2a1ca9bcd8a5ca81f
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 14:49:34 +0100

    1nd commit in Maps and how you can choose the object

--- src/main/java/com/spartaglobal/mavenexample/maps/ExampleMaps.java
+++ src/main/java/com/spartaglobal/mavenexample/maps/ExampleMaps.java
@@ -1,4 +1,20 @@
 package com.spartaglobal.mavenexample.maps;
 
+import com.spartaglobal.mavenexample.collectionexamples.ExampleDeQueues;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExampleMaps {
+    private Map<Integer, String> studentsInClass = new HashMap<>();
+
+    public ExampleMaps(){
+        studentsInClass.put(1,"Anis");
+        studentsInClass.put(2,"Pav");
+        studentsInClass.put(3,"naz");
+    }
+
+    public String returnValue (int keyNum){
+        return studentsInClass.get(keyNum);
+    }
 }
