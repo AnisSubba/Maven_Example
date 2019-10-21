69dc2122ae42855661e5d3b662758878e107b500
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 14:48:10 +0100

    2nd commit in Queues in poll, peek, return

--- src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleQueues.java
+++ src/main/java/com/spartaglobal/mavenexample/collectionexamples/ExampleQueues.java
@@ -1,4 +1,24 @@
 package com.spartaglobal.mavenexample.collectionexamples;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ExampleQueues {
+
+    private Queue<String> stringQueue = new LinkedList<>();
+
+    public ExampleQueues(){
+        stringQueue.add("one");
+        stringQueue.add("two");
+        stringQueue.add("three");
+    }
+
+    public void queueRunner(){
+        System.out.println(stringQueue.peek());
+        System.out.println(stringQueue.poll());
+
+        for(String itemInQueue : stringQueue){
+            System.out.println(itemInQueue);
+        }
+    }
 }
